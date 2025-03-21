5a8924ca3b277acf511e7d42f21cd9a584e0cea8
Josiah <josiah.pilon@gmail.com>
2025-03-20 20:52:29 -0700

    test: new controller action

--- r00lz/spec/r00lz_spec.rb
+++ r00lz/spec/r00lz_spec.rb
@@ -1,11 +1,15 @@
+class TedController < R00lz::Controller
+  def think; "Whoah, man..."; end
+end
+
 RSpec.describe R00lz do
   it "has a version number" do
     expect(R00lz::VERSION).not_to be nil
   end
 
-  it "app returns success" do
+  it "new controller action returns success" do
     env = {
-      "PATH_INFO" => "/",
+      "PATH_INFO" => "/ted/think",
       "QUERY_STRING" => ""
     }
 
